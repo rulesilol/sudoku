0e74b84f7c133e39ee6455236822d9e1b619c864
Andrew <andrewlei323@gmail.com>
2020-08-19 02:32:49 +1000

    Updates

---
+++ precache-manifest.e56e769495aa31292491ce737629657c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00439292cadd2b5dde76c586fa762f70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74bfa3d90dffe814f406",
+    "url": "/static/css/main.17c94d07.chunk.css"
+  },
+  {
+    "revision": "71e32f5487a8fdd1b7e6",
+    "url": "/static/js/2.f811b39c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f811b39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74bfa3d90dffe814f406",
+    "url": "/static/js/main.114d1e61.chunk.js"
+  },
+  {
+    "revision": "c71dd58cd5349d4e6ab8",
+    "url": "/static/js/runtime-main.4dd858cb.js"
+  }
+]);
